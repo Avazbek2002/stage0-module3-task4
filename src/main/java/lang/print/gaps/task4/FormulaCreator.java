3634e565322d31615ba4f6b336fe4e470847161e
Avazbek Isroilov <64166521+Avazbek2002@users.noreply.github.com>
2022-07-20 20:06:50 +0500

    Declaring and (re)assigning the variable

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,5 +2,9 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+        float a = 0, b = 0;
+        float result;
+
+        result = (float)(9 * Math.pow(a, 2) - 5 * b + 2 + a - 7) * ((a + b - 4 * a * b) / 2);
     }
 }
--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,6 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        System.out.println((char)99 + "\n" + (char)104 + "\n" + (char) 97 + "\n" + (char) 114);
     }
 }
